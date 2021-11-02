9a29b7e985bd855c5311f89f95e3278bbe3a4c39
l1nd53y <lndsadams@gmail.com>
2021-11-02 12:05:12 -0500

    More comments

--- src/main/java/com/l1nd53y/mod2/model/User.java
+++ src/main/java/com/l1nd53y/mod2/model/User.java
@@ -8,15 +8,15 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "users")
+@Setter // ^tells Lombok to generate these for all the fields of the class^
+@NoArgsConstructor // generates an empty constructor
+@AllArgsConstructor // generates all-args constructor - requires one argument for every field in the class
+@Entity // represents a table in a relational database
+@Table(name = "users") // allows you to specify the details of the table
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // marks a field as a primary key field
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // @GeneratedValue specifies that the primary key is automatically allocated by DB, GenerationType.IDENTITY ensures id auto-increments
     private long id;
 
     @Column(name = "first_name")
